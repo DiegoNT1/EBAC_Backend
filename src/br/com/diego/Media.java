122998798b65c87bfda802d7a6d0e2a289db3565
DiegoNT1 <diegoagustinhomelo@gmail.com>
2025-04-16 13:30:05 -0300

    first commit

---
+++ src/br/com/diego/Media.java
@@ -0,0 +1,21 @@
+package br.com.diego;
+
+public class Media {
+
+	public static void main(String[] args) {
+		int nota1 = 10;
+		int nota2 = 7;
+		int nota3 = 5;
+		int nota4 = 8;
+		
+		int mediaNotas = (nota1 + nota2 + nota3 + nota4) / 4;	
+		System.out.println("Tipo Primitivo:" + (mediaNotas));
+		
+		
+		Integer mediaWrapper = mediaNotas;
+		
+		System.out.println(" Tipo Wrapper:" + (mediaWrapper));
+
+	}
+
+}
