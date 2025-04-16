ed22421655ba5b2c5c9fa8984aa0bbc731827aa0
DiegoNT1 <diegoagustinhomelo@gmail.com>
2025-04-16 13:25:24 -0300

    first commit

---
+++ src/br/com/diego/Media.java
@@ -0,0 +1,14 @@
+package br.com.diego;
+
+public class Media {
+
+	public static void main(String[] args) {
+		int nota1 = 10;
+		int nota2 = 7;
+		int nota3 = 5;
+		int nota4 = 8;
+		
+		int mediaNotas = (nota1 + nota2 + nota3 + nota4) / 4;	
+		System.out.println(mediaNotas);
+	}
+}
